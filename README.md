e0af341254a3e27d8e7b4e5c51d9bc9156da640b
Devine Lu Linvega <aliceffekt@gmail.com>
2017-12-27 12:18:20 +1300

    Themes preview

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# Themes
+## barber
+![barber](assets/barber.svg)
+
+## battlestation
+![battlestation](assets/battlestation.svg)
+
+## berry
+![berry](assets/berry.svg)
+
+## blanc
+![blanc](assets/blanc.svg)
+
+## cina
+![cina](assets/cina.svg)
+
+## commodore
+![commodore](assets/commodore.svg)
+
+## gameboy
+![gameboy](assets/gameboy.svg)
+
+## mahou
+![mahou](assets/mahou.svg)
+
+## marble
+![marble](assets/marble.svg)
+
+## noir
+![noir](assets/noir.svg)
+
+## pale
+![pale](assets/pale.svg)
+
+## raspberry
+![raspberry](assets/raspberry.svg)
+
+## seaweed
+![seaweed](assets/seaweed.svg)
+
+## swiss
+![swiss](assets/swiss.svg)
+
+## soyuz
+![soyuz](assets/soyuz.svg)
+
---
+++ index.js
@@ -0,0 +1,70 @@
+fs = require('fs');
+var normalizedPath = require("path").join(__dirname, "collection");
+
+var html = ""
+
+function build_svg(n,theme)
+{
+  var name = n.split(".")[0];
+  var size = 16
+
+  var html = `
+  <svg class="vector" width="${(size * 5)}px" height="${size * 3}px" xmlns="http://www.w3.org/2000/svg" baseProfile="full" version="1.1">
+    <rect width='${(size * 5)}' height='${(size * 3)}' fill='${theme.background}' rx='5' ry='5'></rect>
+    <circle cx='${size * 1}' cy='${size * 1}' r='${size/2}' fill='${theme.f_high}'></circle>
+    <circle cx='${size * 2}' cy='${size * 1}' r='${size/2}' fill='${theme.f_med}'></circle>
+    <circle cx='${size * 3}' cy='${size * 1}' r='${size/2}' fill='${theme.f_low}'></circle>
+    <circle cx='${size * 4}' cy='${size * 1}' r='${size/2}' fill='${theme.f_inv}'></circle>
+    <circle cx='${size * 1}' cy='${size * 2}' r='${size/2}' fill='${theme.b_high}'></circle>
+    <circle cx='${size * 2}' cy='${size * 2}' r='${size/2}' fill='${theme.b_med}'></circle>
+    <circle cx='${size * 3}' cy='${size * 2}' r='${size/2}' fill='${theme.b_low}'></circle>
+    <circle cx='${size * 4}' cy='${size * 2}' r='${size/2}' fill='${theme.b_inv}'></circle>
+  </svg>`
+
+  fs.writeFile("assets/"+name+".svg", html, function(err) {
+    if(err) {return console.log(err);}
+    console.log("Saved "+name)
+  }); 
+}
+
+function build_theme(n,theme)
+{
+  var name = n.split(".")[0];
+  return `## ${name}\n![${name}](assets/${name}.svg)\n\n`
+}
+
+function is_json(text)
+{
+  try{
+    JSON.parse(text);
+    return true;
+  }
+  catch (error){
+    return false;
+  }
+}
+
+function generate(html)
+{
+  fs.writeFile("README.md", html, function(err) {
+    if(err) {return console.log(err);}
+    console.log("Done.")
+  }); 
+}
+
+require("fs").readdirSync(normalizedPath).forEach(function(file_name) {
+  fs.readFile('collection/'+file_name, 'utf8', function (err,data){
+    if(err) { return console.log(err); }
+    if(is_json(data)){
+      var theme = JSON.parse(data)
+      build_svg(file_name,theme)
+      html += build_theme(file_name,theme)
+    }
+  });
+});
+
+setTimeout(function(){ generate(html); },1000)
+
+
+
+
